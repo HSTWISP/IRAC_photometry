7c94af50c279c95478a24399d5107edd254ad68a
Ibaronch <ivano_baronchelli@yahoo.it>
2017-08-16 03:25:16 +0200

    shell code to run IRAC_photometry.py on multiple fields

---
+++ run_multiple_IRAC_photometry.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##############################################################
+# WISPIPE - IRAC PHOTOMETRY
+# Ivano Baronchelli, 2017
+#Used to keep track of the fields reduced
+##############################################################
+
+source ~/IRAC_photometry/run_IRAC_photometry.sh 68 A >& LOG/log68.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 79 A >& LOG/log79.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 80 A >& LOG/log80.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 81 A >& LOG/log81.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 83 A >& LOG/log83.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 84 A >& LOG/log84.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 91 A >& LOG/log91.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 95 A >& LOG/log95.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 97 A >& LOG/log97.log
+source ~/IRAC_photometry/run_IRAC_photometry.sh 377 A >& LOG/log377.log
